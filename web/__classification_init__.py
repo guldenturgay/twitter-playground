a6ac522b11885d3d66424d34f91ceb7aa86f1c68
Guldensle <guldenturgay@outlook.com>
2021-06-02 11:35:24 -0700

    removed text cleaning function

--- web/__classification_init__.py
+++ web/__classification_init__.py
@@ -7,35 +7,9 @@ from keras.models import load_model
 import numpy as np
 stopword = nltk.corpus.stopwords.words('english')
 
-model = load_model('./emotion_analysis')
+model = load_model('emotion_analysis')
 
 def predict_emotion(text):
-
-    def clean_text(text):
-        
-        # Replace or remove the characters 
-        replacement_dict={"’":"'", # Replace ’ with '
-                        "'s":" is", # Replace the contractions
-                        "n't": " not",
-                        "'m": " am",
-                        "'ll": " will",
-                        "'d": " would",
-                        "'ve": " have",
-                        "'re": " are",
-                        "\W+": " "} 
-
-        for item in replacement_dict.keys():
-            text=re.sub(item,replacement_dict[item], text)
-
-        text = ''.join([char for char in text if char not in string.punctuation])
-        tokens = re.split('\W+', text)
-        
-        # Remove stopwords
-        text = ' '.join([word for word in tokens if word not in stopword])
-        text=text.lower()
-        return text
-
-
 
     emotion_label_dict = {6: 'neutral',
                             4: 'happy',
@@ -48,14 +22,7 @@ def predict_emotion(text):
                             5: 'joy',
                             9: 'suprise'}
 
-    text = clean_text(text)
-
-
-    def get_emotion(text):
-        probas = model.predict([text])
-        pred = np.argmax(probas)
-        pred = emotion_label_dict[pred]
-        return pred
-
-    predicted = get_emotion(text)
-    return predicted
+    probas = model.predict([text])
+    pred = np.argmax(probas)
+    pred = emotion_label_dict[pred]
+    return pred
