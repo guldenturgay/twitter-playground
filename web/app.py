45509db4dbb5bd3e47c9df7814639604ff8208f2
Guldensle <guldenturgay@outlook.com>
2021-06-02 11:37:22 -0700

    removed request form added prediction func

--- web/app.py
+++ web/app.py
@@ -2,25 +2,34 @@ from flask import Flask, render_template, request
 from wtforms import Form, TextAreaField, validators
 import __classification_init__
 from __classification_init__ import predict_emotion
+import get_tweets
+from get_tweets import get_tweets
 app = Flask(__name__)
 
-class FindEmotionForm(Form):
-    text_ = TextAreaField('', [validators.DataRequired()])
-    
+
+def request_results():
+    tweets = get_tweets()
+    tweets = tweets.tweet_text
+    predictions=[]
+
+    for tweet in tweets:
+        pred = predict_emotion(tweet)
+        predictions.append(pred)
+        emotion = max(set(predictions), key = predictions.count)
+    return emotion
+
+
 @app.route('/')
 def index():
-    form = FindEmotionForm(request.form)
-    return render_template('first_page.html', form=form)
+    output = request_results()
+    return render_template('result.html', output=output)
    
 
-@app.route('/result', methods=['POST'])
-def find_emotion():
-    form = FindEmotionForm(request.form)
-    if request.method == 'POST' and form.validate():
-        text=request.form['text_']
-        output = predict_emotion(text)
-        return render_template('result.html', output=output)
-    return render_template('first_page.html', form=form)
+#@app.route('/result', methods=['GET'])
+#def find_emotion():
+    #if request.method == 'GET':
+        #output = request_results()
+        #return render_template('result.html', output=output)
 
 if __name__ == '__main__':
     app.run(debug=True)
